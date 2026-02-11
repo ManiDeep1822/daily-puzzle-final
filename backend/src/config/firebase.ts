e964d960900bbed2aea50ff2d9b1c8bd9798c445
ManiDeep1822 <indlachinnu123@gmail.com>
2026-02-11 15:21:25 +0530

    fix: use env firebase config for render

--- backend/src/config/firebase.ts
+++ backend/src/config/firebase.ts
@@ -1,8 +1,11 @@
 import admin from "firebase-admin";
-import serviceAccount from "./firebase-service-account.json";
+
+const serviceAccount = JSON.parse(
+  process.env.FIREBASE_SERVICE_ACCOUNT as string,
+);
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
+  credential: admin.credential.cert(serviceAccount),
 });
 
 export const authAdmin = admin.auth();
